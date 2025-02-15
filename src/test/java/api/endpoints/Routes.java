9e8c16012ff4a209fe566d9a7b054c7e2ed3735b
VireshReddy123 <viresh.reddy@sqelabs.com>
2025-02-15 14:36:15 +0530

    Simple

---
+++ src/test/java/api/endpoints/Routes.java
@@ -0,0 +1,22 @@
+package api.endpoints;
+
+public class Routes {
+	
+	
+	//Base URL
+	public static String Base_URL ="https://petstore.swagger.io/v2";
+	
+	//URL for User module.
+	
+	public static String post_Url =Base_URL+"/user";
+	public static String get_Url =Base_URL+"/user/{username}";
+	public static String update_Url =Base_URL+"/user/{username}";
+	public static String delete_Url =Base_URL+"/user/{username}";
+	public static String get_Url1 =Base_URL+"/user/{FirstName}";
+
+
+ 
+
+	
+
+}
